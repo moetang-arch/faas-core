e0aca3317046db44e5f7cd1346b84f74eae994b3
goodplayer <woshiyigelaji@gmail.com>
2018-08-15 23:59:32 +0800

    service

--- faas-service/entrance/main.go
+++ faas-service/entrance/main.go
@@ -1,8 +1,13 @@
 package main
 
-import "runtime"
+import (
+	"runtime"
+	"github.com/moetang-arch/faas-core/faas-service"
+)
 
 func main() {
 	// set defaults
 	runtime.GOMAXPROCS(1)
+
+	service := faas_service.NewService()
 }
